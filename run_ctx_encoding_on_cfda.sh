c104ed380f66098cdee53df773af9d8dad86c2ad
DylanJoo <dylanjootw@gmail.com>
2023-04-06 10:34:35 +0800

    [DR] revise codes of cfda

--- run_ctx_encoding_on_cfda.sh
+++ run_ctx_encoding_on_cfda.sh
@@ -1,7 +1,7 @@
-CORPUS_PREFIX=/home/jhju/datasets/odcqa-wiki-psgs/full_wiki_segments/full_wiki_segments.jsonl
+CORPUS_PREFIX=/home/jhju/datasets/full_wiki_segments/full_wiki_segments.jsonl
 
-for i in 00000..00025;do
-    python -m pyserini.encode input \
+for i in {00000..00025};do
+    python3 -m pyserini.encode input \
       --corpus ${CORPUS_PREFIX}${i} \
       --fields text \
       --shard-id 0 \
@@ -10,6 +10,8 @@ for i in 00000..00025;do
       --to-faiss encoder \
       --encoder facebook/dpr-ctx_encoder-multiset-base \
       --fields text \
-      --batch 32 \
-      --fp16
+      --batch 48 \
+      --fp16 \
+      --device cuda:1
+    echo $CORPUS_PREFIX $i "encoding and indexing is finished." >> log
 done
