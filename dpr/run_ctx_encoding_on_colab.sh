1158e465b9d1f8442e939ba80af47adaaff389e5
朱家輝 <jhju@cfda4.citi.sinica.edu.tw>
2023-04-15 09:06:17 +0800

    [organize] rename files

---
+++ dpr/run_ctx_encoding_on_colab.sh
@@ -0,0 +1,28 @@
+CORPUS_PREFIX=/content/full_wiki_segments/full_wiki_segments.jsonl
+OUTPUT_PREFIX=/content/odcqa-dpr-multiset-bases/dir-
+
+echo "-------\n" >> log
+echo "Run the following shards..." >> log
+
+for i in {00025..00000};do
+    if [ -f "${OUTPUT_PREFIX}${i}"/success ]; then
+        echo ${OUTPUT_PREFIX}${i} "exists and has been done"
+    else
+        python -m pyserini.encode input \
+          --corpus ${CORPUS_PREFIX}${i} \
+          --fields text \
+          --shard-id 0 \
+          --shard-num 1 output \
+          --embeddings ${OUTPUT_PREFIX}${i} \
+          --to-faiss encoder \
+          --encoder facebook/dpr-ctx_encoder-multiset-base \
+          --fields text \
+          --batch 32 \
+          --fp16
+        echo "Shard" $i  >> log
+        touch ${OUTPUT_PREFIX}${i}/success
+        tar cvf /content/odcqa-dpr-multiset-bases-dir-${i}.tar /content/odcqa-dpr-multiset-bases/dir-${i}
+    fi
+done
+
+
