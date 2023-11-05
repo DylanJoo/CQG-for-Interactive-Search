0fe4fefd2e2edd347d80b9218358a6256a855851
DylanJoo <dylanjootw@gmail.com>
2023-11-05 10:27:58 +0800

    update predicting commands

--- predict_fidmrg_qrecc.sh
+++ predict_fidmrg_qrecc.sh
@@ -1,10 +1,12 @@
 COLLECTION=/home/jhju/datasets/wiki.dump.20181220/wiki_psgs_w100.jsonl
 
+RT=bm25
+RT=contriever
 for MODEL in checkpoints/fidmrg*;do
     MODEL_NAME=${MODEL##*/}
     python3 src/inference_fidmrg.py \
-        --jsonl_file data/is4qrecc/qrecc_test_provenances_bm25.jsonl \
-        --output_file evaluation/qrecc_test_pred_bm25.$MODEL_NAME.jsonl \
+        --jsonl_file data/is4qrecc/qrecc_test_provenances_${RT}.jsonl \
+        --output_file evaluation/qrecc_test_pred_${RT}.$MODEL_NAME.jsonl \
         --collections $COLLECTION \
         --used_checkpoint $MODEL/checkpoint-10000 \
         --used_tokenizer google/flan-t5-base \
@@ -12,6 +14,6 @@ for MODEL in checkpoints/fidmrg*;do
         --n_contexts 10 \
         --batch_size 4 \
         --max_length 64 \
-        --device cuda:1 \
+        --device cuda:0 \
         --num_beams 1
 done
